e6094133c992fe7be5748840c5a634e261631630
Nathnael02 <nathnaelyitaferu@yahoo.com>
2022-12-08 07:28:24 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
