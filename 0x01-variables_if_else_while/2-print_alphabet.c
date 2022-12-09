25c8fae9c7f475e05de74f78adb545edb2b7da2e
Nathnael02 <nathnaelyitaferu@yahoo.com>
2022-12-09 12:28:52 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char sl;
+
+	for (sl = 'a'; sl <= 'z'; sl++)
+	{
+		putchar(sl);
+	}
+	putchar('\n');
+	return (0);
+}
