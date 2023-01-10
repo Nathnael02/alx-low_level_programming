cd27d6ac20a47c53f0d28be93c34f5125543b551
Nathnael02 <nathnaelyitaferu@yahoo.com>
2023-01-10 02:38:44 -0800

    task 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,17 +26,13 @@ char *str_concat(char *s1, char *s2)
 	while (s1[i] != '\0')
 
 		i++;
-
 	while (s2[ci] != '\0')
 
 		ci++;
-
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)
-
 		return (NULL);
-
 	i = ci = 0;
 
 	while (s1[i] != '\0')
@@ -45,17 +41,13 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s1[i];
 
 		i++;
-
 	}
-
 	while (s2[ci] != '\0')
 
 	{
 		conct[i] = s2[ci];
 		i++, ci++;
 	}
-
 	conct[i] = '\0';
-
 	return (conct);
 }
